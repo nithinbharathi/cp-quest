877a159ae7270ca003f0ff09182fcbad5bc9ed96
Nithin Bharathi <nithinbharathik@outlook.com>
2023-10-07 18:02:23 +0530

    Create VerticalPaths.java

---
+++ Codeforces/VerticalPaths.java
@@ -0,0 +1,236 @@
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.io.*;
+
+/**
+ * @submission : Nithin Bharathi 07-Oct-2023
+ *
+ *
+ */
+
+public class Problem{
+	static int MOD = 998244353;
+	static long fact[];
+	static ArrayList<Integer>graph[];
+	static boolean vis[];
+	static ArrayList<ArrayList<Integer>>res;
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		Template t = new Template();
+		int test = t.readInt();
+		while(test-->0) {
+			int n = t.readInt();
+			graph = new ArrayList[n];
+			vis = new boolean[n];
+			res = new ArrayList<>();
+			int root = 0;
+			for(int i =0;i<n;i++)graph[i] = new ArrayList<>();
+			for(int i =0;i<n;i++) {
+				int a = t.readInt()-1;
+				if(i == a) {
+					root = i;continue;
+				}
+				graph[a].add(i);graph[i].add(a);
+			}
+			go(root,new ArrayList<Integer>());
+			sb.append(res.size()+"\n");
+			for(ArrayList<Integer>it:res) {
+				sb.append(it.size()+"\n");
+				for(int itr:it)sb.append(itr+" ");
+				sb.append("\n");
+			}
+			sb.append("\n");
+			
+		}
+		System.out.println(sb);
+	}
+	public static void go(int u,ArrayList<Integer>paths) {
+		vis[u] = true;		
+		paths.add(u+1);
+		for(int it:graph[u]) {
+			if(!vis[it]) {
+				go(it,paths);
+			}
+		}
+		if(!paths.isEmpty()) {
+			res.add(new ArrayList<Integer>(paths));
+			paths.clear();
+		}
+
+	}
+
+	
+	public static ArrayList<Pair>expand(int a[],long k){
+		ArrayList<Pair>l = new ArrayList<>();
+		for(int it:a) {
+			long cnt = 1;
+			while(it%k ==0) {
+				it/=k;
+				cnt*=k;
+			}
+			if(!l.isEmpty() && l.get(l.size()-1).x == it) {
+				Pair item = l.get(l.size()-1);
+				item.y+=cnt;
+				l.set(l.size()-1,item);
+			}else l.add(new Pair(it,cnt));
+		}
+		return l;
+	}
+	
+	static class Pair {
+		int x;
+		long y;
+
+		public Pair(int x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(obj == null)return false;
+			
+			Pair o = (Pair)(obj);
+			return this.x == o.x && this.y == o.y;
+		}
+
+	}
+
+	static class Template {
+		private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		private StringTokenizer st;
+
+		public int last(int a[], int target) {
+			int ind = -1;
+			int l = 0, r = a.length - 1;
+			while (l <= r) {
+				int mid = (l + r) / 2;
+				if (a[mid] <= target) {
+					ind = mid;
+					l = mid + 1;
+				} else
+					r = mid - 1;
+			}
+			return ind;
+		}
+
+		public boolean isSorted(int a[]) {
+			for (int i = 0; i < a.length; i++) {
+				if (i + 1 < a.length && a[i] > a[i + 1])
+					return false;
+			}
+			return true;
+		}
+
+		public boolean isPrime(int n) {
+			for (int i = 2; i * i <= n; i++) {
+				if (n % i == 0)
+					return false;
+			}
+			return true;
+		}
+
+		public long factorial(int n) {
+			long fac = 1;
+			for (int i = 1; i <= n; i++)
+				fac *= i;
+			return fac;
+		}
+
+		public ArrayList<Integer> factors(int n) {
+			ArrayList<Integer> l = new ArrayList<>();
+			for (int i = 1; i * i <= n; i++) {
+				if (n % i == 0) {
+					l.add(i);
+					if (n / i != i)
+						l.add(n / i);
+				}
+
+			}
+			return l;
+		}
+
+		public void build(int seg[], int ind, int l, int r, int a[]) {
+			if (l == r) {
+				seg[ind] = a[l];
+				return;
+			}
+			int mid = (l + r) / 2;
+			build(seg, (2 * ind) + 1, l, mid, a);
+			build(seg, (2 * ind) + 2, mid + 1, r, a);
+			seg[ind] = Math.min(seg[(2 * ind) + 1], seg[(2 * ind) + 2]);
+		}
+
+		public long gcd(long a, long b) {
+			return b == 0 ? a : gcd(b, a % b);
+		}
+
+		public int gcd(int a, int b) {
+			return b == 0 ? a : gcd(b, a % b);
+		}
+
+		public void swap(int a[], int i, int j) {
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+
+		public void swap(char a[], int i, int j) {
+			char t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+
+		public void swap(ArrayList<Integer> l, int i, int j) {
+			int t = l.get(i);
+			l.set(i, l.get(j));
+			l.set(j, t);
+		}
+
+		public void sort(int arr[]) {
+			ArrayList<Integer> list = new ArrayList<>();
+			for (int it : arr)
+				list.add(it);
+			Collections.sort(list);
+			int z = 0;
+			for (int i = 0; i < arr.length; i++)
+				arr[z++] = list.get(i);
+		}
+
+		public void sort(long arr[]) {
+			ArrayList<Long> list = new ArrayList<>();
+			for (long it : arr)
+				list.add(it);
+			Collections.sort(list);
+			int z = 0;
+			for (int i = 0; i < arr.length; i++)
+				arr[z++] = list.get(i);
+		}
+
+		public String read() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		public int readInt() {
+			return Integer.parseInt(read());
+		}
+
+		public long readLong() {
+			return Long.parseLong(read());
+		}
+
+		public double readDouble() {
+			return Double.parseDouble(read());
+		}
+
+	}
+
+}
